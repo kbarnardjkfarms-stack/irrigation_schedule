8aa67c9f2df0d8a71ba1ca5e06c672f90baf2d3e
kbarnardjkfarms-stack <kbarnardjkfarms@gmail.com>
2026-07-09 15:45:37 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,23 @@
-{
-  "hosting": {
-    "public": "dist",
-    "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
-    "rewrites": [{ "source": "**", "destination": "/index.html" }]
-  },
-  "firestore": {
-    "rules": "firestore.rules"
-  },
-  "functions": {
-    "source": "functions"
-  }
+import { initializeApp } from 'firebase/app'
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOdc9DPdn2YqW3zG87Qzaj50TmteW4iBY',
+  authDomain: 'irrigation-schedule-f8b69.firebaseapp.com',
+  projectId: 'irrigation-schedule-f8b69',
+  storageBucket: 'irrigation-schedule-f8b69.firebasestorage.app',
+  messagingSenderId: '1016145843540',
+  appId: '1:1016145843540:web:c7112a3d7061f5e31064c1'
 }
+
+const app = initializeApp(firebaseConfig)
+
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager()
+  })
+})
