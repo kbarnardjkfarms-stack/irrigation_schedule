6cff1e52b987e7647ade824a64ca04d220f67aa1
DevinEastin <crystalclear767@gmail.com>
2026-07-31 15:25:24 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ import {
   persistentLocalCache,
   persistentMultipleTabManager
 } from 'firebase/firestore'
-
+import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: 'AIzaSyBOdc9DPdn2YqW3zG87Qzaj50TmteW4iBY',
   authDomain: 'irrigation-schedule-f8b69.firebaseapp.com',
@@ -13,11 +13,10 @@ const firebaseConfig = {
   messagingSenderId: '1016145843540',
   appId: '1:1016145843540:web:c7112a3d7061f5e31064c1'
 }
-
 const app = initializeApp(firebaseConfig)
-
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({
     tabManager: persistentMultipleTabManager()
   })
 })
+export const auth = getAuth(app)
